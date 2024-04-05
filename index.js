a5d71099176ea48c7e3a974893c3b644bb955d7a
denisbuarque <denisprogramadorweb@gmail.com>
2024-04-05 17:09:05 -0300

    Criando a conexão com o mysql e post de pergunta

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+//Database
+const connection = require('./database/connection');
+// Test connection database
+connection.authenticate().then(() => {
+    console.log('Conectado ao bando Mysql');
+}).catch((error) => {
+    console.log(error);
+});
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Guia de perguntas e respostas.');
-})
+// files statics
+app.use(express.static('public'));
+// encoded data
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+// routes
+app.get('/', (req, res) => {
+    res.send('Questions and response guide.');
+});
+
+app.get('/questions', (req, res) => {
+    res.send("Lista de perguntas e respostas:");
+});
+
+app.post('/question/store', (req, res) => {
+    const { title, description } = req.body;
+
+    const object = {
+        title,
+        description
+    }
+
+    console.log(object);
+    
+    res.send("Dados inseridos com sucesso!");
+});
+
+// run server
 app.listen(5000, () => {
-    console.log("Servidor rodando na porta 5000.");
+    console.log("Server running at port 5000.");
 });
